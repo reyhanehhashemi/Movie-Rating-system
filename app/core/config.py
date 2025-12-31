dd55605b3685ed91df23271d75d32288cd1a3ed1
sanazzsanaz <your-email@example.com>
2025-12-31 12:29:02 +0330

    chore: update application config

---
+++ app/core/config.py
@@ -0,0 +1,16 @@
+from functools import lru_cache
+
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+
+    DATABASE_URL: str = "postgresql://reyhanehhashemi@localhost:5432/movies_db"
+
+    class Config:
+        env_file = ".env"
+
+
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
